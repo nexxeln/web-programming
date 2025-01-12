0bbaed1df1140f6fa1dddf06b5420b9a658b8e7d
Shoubhit Dash <shoubhit2005@gmail.com>
2025-01-12 21:26:12 +0530

    mmm

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const exerciseList = document.getElementById("exercise-list");
 exercises.forEach((num) => {
   const li = document.createElement("li");
   const a = document.createElement("a");
-  a.href = `/${num}`;
+  a.href = `/${num}.html`;
   a.textContent = `Exercise ${num}`;
   li.appendChild(a);
   exerciseList.appendChild(li);
